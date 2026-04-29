c727b5e38c483bf24afff31cfa7959fbb0e640ef
JJ Kasper <jj@jjsweb.site>
2026-04-29 15:32:31 -0700

    fix: ignore example environment variables (#60)
    * fix: ignore example environment variables
    
    * add changeset
    
    * use allowed nextjs list
    
    * lint
    
    ---------
    
    Co-authored-by: ceolinwill <4393133+ceolinwill@users.noreply.github.com>

--- packages/adapter/src/outputs.ts
+++ packages/adapter/src/outputs.ts
@@ -345,8 +345,15 @@ async function getProjectEnvFiles(projectDir: string): Promise<string[]> {
   const envFiles: string[] = [];
   const projectFiles = (await fs.readdir(projectDir).catch(() => [])).sort();
 
+  const productionEnvFiles = new Set([
+    '.env.production.local',
+    '.env.local',
+    '.env.production',
+    '.env',
+  ]);
+
   for (const file of projectFiles) {
-    const isEnv = file === '.env' || file.startsWith('.env.');
+    const isEnv = productionEnvFiles.has(file);
 
     if (!isEnv) {
       continue;
